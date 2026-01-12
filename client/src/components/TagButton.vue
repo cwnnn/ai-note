bc52c255e89ca7f61b83ef497db1b21e512526f4
cwnnn <cangs026@gmail.com>
2026-01-12 22:11:51 +0300

    add tagbutton

---
+++ client/src/components/TagButton.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  removable: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits(["click", "remove"]);
+</script>
+
+<template>
+  <button
+    type="button"
+    class="rounded-full border text-center border-white bg-gray-200/10 py-1 px-3 text-xs duration-300 hover:scale-105"
+    @click="emit('click')"
+  >
+    <slot>
+      <span>#{{ label }}</span>
+
+      <span
+        v-if="removable"
+        class="ml-1 cursor-pointer rounded-full px-1 text-xs duration-300 hover:text-red-300"
+        @click.stop="emit('remove')"
+      >
+        ✕
+      </span>
+    </slot>
+  </button>
+</template>
