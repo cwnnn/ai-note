718aa45bea5445fb0ab4343a7a00e4b2b67517cb
cwnnn <cangs026@gmail.com>
2026-01-12 22:10:26 +0300

    add header

---
+++ client/src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="header">
+    <div class="header-wrapper">
+      <!-- SOL -->
+      <div class="header-left">
+        <slot name="logo">
+          <!-- default logo -->
+          <div class="header-logo">⚡</div>
+          <h1 class="header-title">App Name</h1>
+        </slot>
+      </div>
+
+      <!-- SAĞ -->
+      <div class="header-right">
+        <!-- Desktop Nav -->
+        <nav class="header-nav-desktop">
+          <slot name="nav" />
+        </nav>
+
+        <!-- Mobile Button -->
+        <button @click="mobileOpen = !mobileOpen" class="header-mobile-btn">
+          ☰
+        </button>
+      </div>
+      <!-- Actions -->
+      <div class="header-right">
+        <slot name="actions" />
+      </div>
+    </div>
+
+    <!-- MOBILE MENU -->
+    <div v-if="mobileOpen" class="header-mobile">
+      <nav class="header-mobile-nav">
+        <slot name="nav" :close="closeMobile" />
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const mobileOpen = ref(false);
+const closeMobile = () => {
+  mobileOpen.value = false;
+};
+</script>
+<style src="./AppHeader.css"></style>
